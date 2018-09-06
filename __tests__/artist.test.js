75320f4f1ec252dba4089079ec6965deed54694c
Nick-Damico <nickalan82@icloud.com>
2018-09-05 22:44:04 -0400

    rename test directory to __tests__ per documentation, removed mock from artist.test.js, and implemented (3) tests for Artist class

--- __tests__/artist.test.js
+++ __tests__/artist.test.js
@@ -1,10 +1,5 @@
-import Artist from '../lib/Artist';
-jest.mock('../lib/Artist');
+import Artist from '../lib/artist';
 
-beforeEach(() => {
-
-  Artist.mockClear();
-});
 
 describe('Artist Class', () => {
 
@@ -13,4 +8,18 @@ describe('Artist Class', () => {
     expect(newArtist).toBeTruthy();
   });
 
-})
+  it('can be instantiated with a name for the artist', () => {
+    const newArtist = new Artist('Michael Jackson');
+    expect(newArtist.name).toBe('Michael Jackson');
+  });
+
+  describe('#setName', () => {
+
+    it("it sets the artist's name", () => {
+      const newArtist = new Artist();
+      newArtist.setName('Alan Jackson');
+      expect(newArtist.name).toBe('Alan Jackson');
+    });
+  });
+
+});
