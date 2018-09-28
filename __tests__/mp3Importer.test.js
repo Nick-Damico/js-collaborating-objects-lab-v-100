fe7d90786fbdd86fd2cfa7a7d5f71be6cf0c5fa0
Nick-Damico <nickalan82@icloud.com>
2018-09-28 17:55:33 -0400

    add three tests that describe the behavoir of the MP3Importer class, and its constructor operations. Two of the 3 tests are passing with solutions in lib/mp3importer.js

--- __tests__/mp3Importer.test.js
+++ __tests__/mp3Importer.test.js
@@ -1,2 +1,30 @@
 import Song from '../lib/Song';
 import Artist from '../lib/Artist';
+import MP3Importer from '../lib/MP3Importer';
+
+describe ('MP3Importer', () => {
+  describe ('#constructor', () => {
+    it('it initializes with a filepath argument to parse mp3 files from', () => {
+      const musicPath = '../fixtures/mp3s';
+      const mp3ImporterOne = new MP3Importer(musicPath);
+
+      expect(mp3ImporterOne.path).toEqual(musicPath);
+    });
+  });
+
+  describe ('files', () => {
+    it ('initializes a property of files equal to an array', () => {
+      const musicPath = '../fixtures/mp3s';
+      const mp3ImporterOne = new MP3Importer(musicPath);
+
+      expect(mp3ImporterOne.files).toBeInstanceOf(Array);
+    });
+
+    it ('loads all mp3 files in the path directory', () => {
+      const musicPath = '../fixtures/mp3s';
+      const mp3ImporterOne = new MP3Importer(musicPath);
+
+      expect(mp3ImporterOne.files.length).toEqual(5);
+    });
+  });
+});
